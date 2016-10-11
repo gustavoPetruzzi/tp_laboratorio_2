8af3b2b8217d1182bf9a59da5002e3c8df2c0e81
mavocado3 <gustavopetruzzi19@gmail.com>
2016-10-10 22:04:46 -0300

    Add files via upload

---
+++ TP2/Clase_12_Library/Camion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Clase_12_Library_2;
+
+namespace Clase_12_Library
+{
+    public class Camion:Vehiculo
+    {
+        
+        public Camion(EMarca marca, string patente, ConsoleColor color)
+            : base(patente, marca, color)
+        {
+        }
+        /// <summary>
+        /// Los camiones tienen 8 ruedas
+        /// </summary>
+        public override short CantidadRuedas
+        {
+            get
+            {
+                return 8;
+            }
+            set
+            {
+            }
+
+        }
+        /// <summary>
+        /// Mostrara la informacion del  Camion: Patente, color, marca y cantidad de ruedas.
+        /// </summary>
+        /// <returns></returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("CAMION");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendFormat("RUEDAS : {0}", this.CantidadRuedas);
+            sb.AppendLine("");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
